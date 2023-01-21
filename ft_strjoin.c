d7b83bc7b2816a07d00cba57d2b5d44c2e5fc040
Elbakyan Shirak <elbakyan-2000@mail.ru>
2023-01-21 10:57:29 +0400

    join

---
+++ ft_strjoin.c
@@ -0,0 +1,20 @@
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	len_result_string;
+	char	*ptr_result;
+	char	*result;
+
+	len_result_string = ft_strlen(s1) + ft_strlen(s2) + 1;
+	ptr_result = (char *) malloc(len_result_string * sizeof(char));
+	if (!ptr_result)
+		return (NULL);
+	result = ptr_result;
+	while (*s1 != '\0')
+		*ptr_result++ = *s1++;
+	while (*s2 != '\0')
+		*ptr_result++ = *s2++;
+	*ptr_result =  '\0';
+	return (result);
+}
