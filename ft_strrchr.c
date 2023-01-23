db31bfded235a05d791ee9cbfadc313ee1b068e4
Shirak Elbakyan <selbakya@c2r1s1.42yerevan.am>
2023-01-23 21:21:30 +0400

    fff

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: selbakya <selbakya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 21:23:28 by selbakya          #+#    #+#             */
-/*   Updated: 2023/01/23 20:43:04 by selbakya         ###   ########.fr       */
+/*   Updated: 2023/01/23 21:21:09 by selbakya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,15 @@ char    *ft_strrchr(const char *s, int symb)
     char    *start;
     
     start = (char *)s;
-    while (*s++)
+    while (*s)
     {
         while (--s != start && *s != (char)symb)
         {
             if (*s == (char)symb)
                 return ((char *)s);
+            s--;
         }
+        s++;
     }
     return (NULL);
 }
