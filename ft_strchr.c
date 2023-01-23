d5a738041689ed9be223c990bb5cffc6dbbdd139
Shirak Elbakyan <selbakya@c2r1s1.42yerevan.am>
2023-01-23 21:13:38 +0400

    wtf

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,21 +6,53 @@
 /*   By: selbakya <selbakya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 20:50:27 by selbakya          #+#    #+#             */
-/*   Updated: 2023/01/19 21:05:30 by selbakya         ###   ########.fr       */
+/*   Updated: 2023/01/23 21:08:45 by selbakya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*char	*ft_strchr(const char *str, int ch)
+{
+	int	i;
+
+	i = 0;
+	if (str[i] == (unsigned char)ch)
+		return ((char *)&str[i]);
+	while (str[i] != 0)
+	{
+		if (str[i + 1] == (unsigned char)ch)
+			return ((char *)&str[i + 1]);
+		i++;
+	}
+	return (NULL);
+}
+*/
+
 char    *ft_strchr(const char *pointer_str, int symbol)
 {
 	unsigned char c;
+    size_t  iterator;
 
+    iterator = 0;
 	c = (unsigned char)symbol;
-    while (++pointer_str)
-        {
-            if (*pointer_str == c)
-                return ((char *)pointer_str);
-        }
+    while (pointer_str[iterator] != 0) 
+    {
+        if (pointer_str[iterator] == c)
+            return ((char *)&pointer_str[iterator]);
+        iterator++;
+    }
     return (NULL);
 }
+
+
+/*	signal(SIGSEGV, sigsegv);
+	title("ft_strchr\t: ")
+	char s[] = "tripouille";
+	check(ft_strchr(s, 't') == s); showLeaks(); 
+
+	check(ft_strchr(s, 'z') == 0); showLeaks();
+
+	check(ft_strchr(s, 't' + 256) == s); showLeaks();
+	write(1, "\n", 1);
+*/ 
