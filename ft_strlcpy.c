a28a28c4d2935085b37778df91907baa8e74b14b
Shirak Elbakyan <selbakya@c1r4s5.42yerevan.am>
2023-01-24 16:01:00 +0400

    teses are OK

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: selbakya <selbakya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 15:54:29 by selbakya          #+#    #+#             */
-/*   Updated: 2023/01/24 14:37:43 by selbakya         ###   ########.fr       */
+/*   Updated: 2023/01/24 15:59:13 by selbakya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,10 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 	counter = 0;
 	if (size > 0)
 	{
-		while (size && src[++counter])
+		while (--size && src[counter])
 		{
 			dest[counter] = src[counter];	
 			counter++;
-			size--;
 		}
 		dest[counter] = '\0';
 	}
@@ -31,14 +30,3 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 		counter++;
 	return (counter);
 }
-
-		while (--size && src[i])
-		{
-			dst[i] = src[i];
-			i++;
-		}
-		dst[i] = '\0';
-	}
-	while (src[i])
-		i++;
-	return (i);
