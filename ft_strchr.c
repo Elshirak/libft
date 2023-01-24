9b02b66f6e42896de6f2e923c4350c4c2e55ca33
Shirak Elbakyan <selbakya@c2r6s4.42yerevan.am>
2023-01-24 13:58:20 +0400

    tested

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,62 +6,24 @@
 /*   By: selbakya <selbakya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 20:50:27 by selbakya          #+#    #+#             */
-/*   Updated: 2023/01/23 21:08:45 by selbakya         ###   ########.fr       */
+/*   Updated: 2023/01/24 13:56:41 by selbakya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*char	*ft_strchr(const char *str, int ch)
-{
-	int	i;
-
-	i = 0;
-	if (str[i] == (unsigned char)ch)
-		return ((char *)&str[i]);
-	while (str[i] != 0)
-	{
-		if (str[i + 1] == (unsigned char)ch)
-			return ((char *)&str[i + 1]);
-		i++;
-	}
-	return (NULL);
-}
-*/
-
-	if (*pointer == '\0' && c == '\0')
-	{
-		return (pointer);
-	}
-
-
-
 char    *ft_strchr(const char *pointer_str, int symbol)
 {
 	unsigned char c;
-    size_t  iterator;
 
-    iterator = 0;
 	c = (unsigned char)symbol;
-    while (pointer_str[iterator] != 0) 
+	if (symbol == 0)
+		return ((char *)(pointer_str + ft_strlen(pointer_str)));
+    while (*pointer_str != 0) 
     {
-        if (pointer_str[iterator] == c)
-            return ((char *)&pointer_str[iterator]);
-        iterator++;
+        if (*pointer_str == c)
+            return ((char *)pointer_str);
+        pointer_str++;
     }
-	if (pointer_str[iterator] == '\0' && c == '\0')
-		return (pointer_str[iterator]);
     return (NULL);
 }
-
-
-/*	signal(SIGSEGV, sigsegv);
-	title("ft_strchr\t: ")
-	char s[] = "tripouille";
-	check(ft_strchr(s, 't') == s); showLeaks(); 
-
-	check(ft_strchr(s, 'z') == 0); showLeaks();
-
-	check(ft_strchr(s, 't' + 256) == s); showLeaks();
-	write(1, "\n", 1);
-*/ 
