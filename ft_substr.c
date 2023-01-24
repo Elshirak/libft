f596a0208cb3069989d347ddab1113fbeb1fc84b
Shirak Elbakyan <selbakya@c2r7s2.42yerevan.am>
2023-01-24 20:02:10 +0400

    Tests are OK

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: selbakya <selbakya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 15:25:14 by selbakya          #+#    #+#             */
-/*   Updated: 2023/01/22 15:37:27 by selbakya         ###   ########.fr       */
+/*   Updated: 2023/01/24 20:01:16 by selbakya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,14 @@ char    *ft_substr(char const *s, unsigned int start, size_t len)
     size_t  string_size;
     char    *ptr_to_result_string;
     size_t  iterator;
-    
+            
+
     string_size = ft_strlen(&s[start]);
     if (string_size >= len)
         string_size = len;
     
+    if ((size_t)start > ft_strlen(s))
+        string_size = 0;
     ptr_to_result_string = (char *) malloc(string_size * sizeof (char));
     if (!ptr_to_result_string)
         return (NULL);
