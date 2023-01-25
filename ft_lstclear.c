d5451c4baea7c1f22beb17480e1950e89beb300d
Shirak Elbakyan <selbakya@c2r7s2.42yerevan.am>
2023-01-25 20:18:42 +0400

    Tested OK

---
+++ ft_lstclear.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: selbakya <selbakya@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/25 19:46:38 by selbakya          #+#    #+#             */
+/*   Updated: 2023/01/25 19:53:14 by selbakya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+    t_list  *node;
+    
+    if (!lst || !del)
+        return ;
+    while (*lst)
+    {
+        node = (*lst) -> next;
+        ft_lstdelone(*lst, del);
+        *lst = node;
+    }
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: selbakya <selbakya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 20:48:37 by selbakya          #+#    #+#             */
-/*   Updated: 2023/01/25 19:12:33 by selbakya         ###   ########.fr       */
+/*   Updated: 2023/01/25 20:10:42 by selbakya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,10 @@ typedef struct s_list {
 	struct s_list	*next;
 }	t_list;
 
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void    ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstadd_back(t_list **lst, t_list *new);
 t_list	*ft_lstlast(t_list *lst);
 int		ft_lstsize(t_list *lst);
