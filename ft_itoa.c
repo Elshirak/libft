38390a9abeea87b56c67be80aa5d651101c1e4e2
Shirak Elbakyan <selbakya@c1r4s5.42yerevan.am>
2023-01-25 15:32:04 +0400

    dfghjk

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,38 +6,74 @@
 /*   By: selbakya <selbakya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 15:08:56 by selbakya          #+#    #+#             */
-/*   Updated: 2023/01/25 14:07:26 by selbakya         ###   ########.fr       */
+/*   Updated: 2023/01/25 15:30:35 by selbakya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_getsize(const int sign, const int n);
+long	ft_getsize(const int n);
+char	*ft_fillstr(int n, char *result_str, long size_str);
+
 
 char	*ft_itoa(int n)
 {
-	int		sign;
-	size_t	size;
-	char	*ptr;
+	long	size_str;
+	char	*result_str;
 
-	sign = 1;
-	if (n < 0)
-		sign = -1;
-	size = ft_getsize(sign, n);
-	ptr = (char *) malloc ((size) * sizeof (char));
+	/*
 	if (n == -2147483648)
 		return (ft_strdup("-2147483648"));
-
+	if (n == 2147483647)
+		return (ft_strdup("2147483647"));
+	if (n == 0)
+		return (ft_strdup("0"));
+	*/
+	size_str = ft_getsize(n);
+	result_str = (char *) malloc(size_str * sizeof (char));
+	if (result_str == NULL)
+		return (NULL);
+	return (ft_fillstr(n, result_str, size_str));
+	
 }
 
-size_t	ft_getsize(const int sign, const int n)
+char	*ft_fillstr(int n, char *result_str, long size_str)
+{	
+	if (n == 0)
+	{
+		result_str[0] = 48;
+		return (result_str);
+	}
+	if (n < 0)
+	{
+		result_str[0] = '-';
+		n = n * -1;
+		size_str--;
+	}
+	printf("%s RESULT_STR\n", result_str);
+	printf("%ld SIZE_STR\n", size_str);
+	printf("%d N\n\n", n);
+	while (n > 0)
+	{
+		result_str[size_str] = 48 + (n % 10);
+		printf("%s RESULT_STR\n", result_str);
+		n = n / 10;
+		size_str--;
+		printf("%ld SIZE_STR\n", size_str);
+		printf("%d N\n", n);
+		
+	}
+	return (result_str);
+}
+
+long	ft_getsize(const int n)
 {
-	size_t	size;
+	long	size;
 	int		m;
 
 	m = (int)n;
 	size = 0;
-	if (sign == -1)
+	if (m < 0)
 	{
 		size++;
 		m *= -1;
@@ -47,7 +83,51 @@ size_t	ft_getsize(const int sign, const int n)
 		m /= 10;
 		size++;
 	}
-	return (size + 1);
+	return (size);
 }
 
- 
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%s\n", ft_itoa(42));
+	return (0);
+}
+
+
+/*
+ size_t	size;
+	size_t	iterator;
+	char	*result_string;
+
+	size = ft_getsize(n);
+	result_string = (char *) malloc ((size) * sizeof (char));
+	if (!result_string)
+		return (NULL);
+	iterator = 0;
+	result_string[size] = '\0';
+	if (n == 0)
+	{
+		result_string[0] = 48;
+		return (result_string);
+	}
+	if (n < 0)
+	{
+		result_string[0] = '-';
+		n = n * -1;
+	}
+	while (n >= 0)
+	{
+		result_string[iterator] = 48 + (n % 10);
+		n = n / 10;
+		iterator--;
+	}
+	return (result_string);
+
+	printf("%s RESULT_STR\n", result_str);
+	printf("%ld SIZE_STR\n", size_str);
+	printf("%d N\n", n);
+
+	*/
