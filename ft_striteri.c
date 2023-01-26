daf7c9e7462e10d5c18620d03f0d7de3ed99031a
Shirak Elbakyan <selbakya@c2r8s5.42yerevan.am>
2023-01-26 22:01:11 +0400

    Tests OK Norm OK

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,23 +6,23 @@
 /*   By: selbakya <selbakya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 12:33:58 by selbakya          #+#    #+#             */
-/*   Updated: 2023/01/25 12:36:53 by selbakya         ###   ########.fr       */
+/*   Updated: 2023/01/26 22:00:53 by selbakya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    ft_striteri(char *s, void (*f)(unsigned int, char*))
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
-    size_t  iterator;
-    
-    if (!s || !f)
-        return ;
-    iterator = 0;
-    while (s[iterator])
-    {
-        f(iterator, s + iterator);
-        iterator++;
-    }    
-    return ;
+	size_t	iterator;
+
+	if (!s || !f)
+		return ;
+	iterator = 0;
+	while (s[iterator])
+	{
+		f(iterator, s + iterator);
+		iterator++;
+	}
+	return ;
 }
