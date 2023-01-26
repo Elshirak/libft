f2d7f10d26433376ddc3ba10d6a33d8af3d647ed
Shirak Elbakyan <selbakya@c2r8s5.42yerevan.am>
2023-01-26 21:29:59 +0400

    Tests OK Norm OK

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: selbakya <selbakya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 19:46:38 by selbakya          #+#    #+#             */
-/*   Updated: 2023/01/25 19:53:14 by selbakya         ###   ########.fr       */
+/*   Updated: 2023/01/26 21:29:39 by selbakya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
-    t_list  *node;
-    
-    if (!lst || !del)
-        return ;
-    while (*lst)
-    {
-        node = (*lst) -> next;
-        ft_lstdelone(*lst, del);
-        *lst = node;
-    }
+	t_list	*node;
+
+	if (!lst || !del)
+		return ;
+	while (*lst)
+	{
+		node = (*lst)-> next;
+		ft_lstdelone(*lst, del);
+		*lst = node;
+	}
 }
