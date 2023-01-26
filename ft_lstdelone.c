d11be9b5b5765a00dfed4ede7c52fb0126e4c1c0
Shirak Elbakyan <selbakya@c2r8s5.42yerevan.am>
2023-01-26 21:31:24 +0400

    Test OK Norm OK

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,17 +6,17 @@
 /*   By: selbakya <selbakya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 19:35:27 by selbakya          #+#    #+#             */
-/*   Updated: 2023/01/25 19:44:09 by selbakya         ###   ########.fr       */
+/*   Updated: 2023/01/26 21:30:52 by selbakya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    ft_lstdelone(t_list *lst, void (*del)(void *))
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-    if (lst && del)
-    {
-        (*del)(lst -> content);
-        free(lst);
-    }
+	if (lst && del)
+	{
+		(*del)(lst -> content);
+		free(lst);
+	}
 }
