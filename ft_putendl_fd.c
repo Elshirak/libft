2d68d96a86f5eb97924b77b4a67808f666765023
Shirak Elbakyan <selbakya@c2r8s5.42yerevan.am>
2023-01-26 21:51:29 +0400

    Tests OK Norm OK

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,14 +6,14 @@
 /*   By: selbakya <selbakya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 21:24:00 by selbakya          #+#    #+#             */
-/*   Updated: 2023/01/24 21:25:57 by selbakya         ###   ########.fr       */
+/*   Updated: 2023/01/26 21:51:13 by selbakya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    ft_putendl_fd(char *s, int fd)
+void	ft_putendl_fd(char *s, int fd)
 {
-    ft_putstr_fd(s, fd);
-    write(fd, &"\n", 1);
+	ft_putstr_fd(s, fd);
+	write(fd, &"\n", 1);
 }
