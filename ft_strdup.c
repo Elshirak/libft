e8721d12d304a7d644e244abd990d12a9f9df024
Shirak Elbakyan <selbakya@c2r8s5.42yerevan.am>
2023-01-26 21:59:57 +0400

    Tests OK Norm OK

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,25 +6,24 @@
 /*   By: selbakya <selbakya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 15:07:50 by selbakya          #+#    #+#             */
-/*   Updated: 2023/01/24 18:51:49 by selbakya         ###   ########.fr       */
+/*   Updated: 2023/01/26 21:59:41 by selbakya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strdup(const char *source_string)
+char	*ft_strdup(const char *source_string)
 {
 	size_t	length_str;
 	size_t	i;
 	char	*result;
-	
+
 	length_str = ft_strlen(source_string) + 1;
 	result = (char *) malloc(sizeof(char) * length_str);
-
 	if (!result)
 		return (NULL);
 	i = 0;
-	while ( i < length_str)
+	while (i < length_str)
 	{
 		result[i] = source_string[i];
 		i++;
