1b8913496927b2bef10009b55570905040f08bd2
Shirak Elbakyan <selbakya@c2r8s5.42yerevan.am>
2023-01-26 21:36:25 +0400

    Test OK Norm OK

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: selbakya <selbakya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 20:10:52 by selbakya          #+#    #+#             */
-/*   Updated: 2023/01/26 20:07:40 by selbakya         ###   ########.fr       */
+/*   Updated: 2023/01/26 21:36:03 by selbakya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,23 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-    t_list  *ptr;
-    t_list  *first_map;
-    
-    if (!lst)
-        return (NULL);
-    first_map = NULL;
-    while (lst)
-    {
-        ptr = ft_lstnew(f(lst->content));
-        if (!ptr)
-        {
-            ft_lstclear(&first_map, del);
-            return (NULL);
-        }
-        ft_lstadd_back(&first_map, ptr);
-        ptr = ptr->next;
-        lst = lst->next;
-    }
-    return (first_map);
+	t_list	*ptr;
+	t_list	*first_map;
+
+	if (!lst)
+		return (NULL);
+	first_map = NULL;
+	while (lst)
+	{
+		ptr = ft_lstnew(f(lst->content));
+		if (!ptr)
+		{
+			ft_lstclear(&first_map, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&first_map, ptr);
+		ptr = ptr->next;
+		lst = lst->next;
+	}
+	return (first_map);
 }
