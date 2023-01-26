b3de029dbf891a3a4aa41b56def935d69cb99181
Shirak Elbakyan <selbakya@c2r8s5.42yerevan.am>
2023-01-26 21:48:46 +0400

    Test OK Norm OK

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,28 +6,27 @@
 /*   By: selbakya <selbakya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 01:41:04 by selbakya          #+#    #+#             */
-/*   Updated: 2023/01/25 17:32:14 by selbakya         ###   ########.fr       */
+/*   Updated: 2023/01/26 21:48:22 by selbakya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_memcmp(const void *array1, const void *array2, size_t len)
+int	ft_memcmp(const void *array1, const void *array2, size_t len)
 {
-    unsigned char *arr1;
-    unsigned char *arr2;
-    size_t        iterable;
-    
-    iterable = 0;
-    arr1 = (unsigned char *)array1;
-    arr2 = (unsigned char *)array2;
-    
-    if (!len)
-        return (0);
-    while (++iterable < len && *arr1 == *arr2)
-    {
-            ++arr1;
-            ++arr2;    
-    }
-    return ((int)(*arr1 - *arr2));
+	unsigned char	*arr1;
+	unsigned char	*arr2;
+	size_t			iterable;
+
+	iterable = 0;
+	arr1 = (unsigned char *)array1;
+	arr2 = (unsigned char *)array2;
+	if (!len)
+		return (0);
+	while (++iterable < len && *arr1 == *arr2)
+	{
+		++arr1;
+		++arr2;
+	}
+	return ((int)(*arr1 - *arr2));
 }
