7a2fa20d14456d434c1d7e83b62fbb227539c9fd
Shirak Elbakyan <selbakya@c2r8s5.42yerevan.am>
2023-01-26 22:17:39 +0400

    Tests OK Norm OK

--- ft_substr.c
+++ ft_substr.c
@@ -6,36 +6,34 @@
 /*   By: selbakya <selbakya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 15:25:14 by selbakya          #+#    #+#             */
-/*   Updated: 2023/01/25 16:55:08 by selbakya         ###   ########.fr       */
+/*   Updated: 2023/01/26 22:17:22 by selbakya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char    *ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-    size_t  string_size;
-    char    *ptr_to_result_string;
-    size_t  iterator;
-            
-    if (s == 0)
-        return (NULL);
-    string_size = ft_strlen(&s[start]);
-    if (string_size >= len)
-        string_size = len;
-    
-    if ((size_t)start > ft_strlen(s))
-        string_size = 0;
-    ptr_to_result_string = (char *) malloc((string_size + 1)* sizeof (char));
-    if (!ptr_to_result_string)
-        return (NULL);
-    
-    iterator = 0;
-    while (iterator != string_size)
-    {
-        ptr_to_result_string[iterator] = s[iterator + start];
-        iterator++;
-    }
-    ptr_to_result_string[iterator] = '\0';
-    return (ptr_to_result_string);
+	size_t	string_size;
+	char	*ptr_to_result_string;
+	size_t	iterator;
+
+	if (s == 0)
+		return (NULL);
+	string_size = ft_strlen(&s[start]);
+	if (string_size >= len)
+		string_size = len;
+	if ((size_t)start > ft_strlen(s))
+		string_size = 0;
+	ptr_to_result_string = (char *) malloc((string_size + 1) * sizeof (char));
+	if (!ptr_to_result_string)
+		return (NULL);
+	iterator = 0;
+	while (iterator != string_size)
+	{
+		ptr_to_result_string[iterator] = s[iterator + start];
+		iterator++;
+	}
+	ptr_to_result_string[iterator] = '\0';
+	return (ptr_to_result_string);
 }
