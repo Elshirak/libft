85dae76351cf37a6773f3237904632712756f2c6
Shirak Elbakyan <selbakya@c2r8s5.42yerevan.am>
2023-01-26 21:27:39 +0400

    Tests OK Norm OK

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,16 +6,16 @@
 /*   By: selbakya <selbakya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 18:42:01 by selbakya          #+#    #+#             */
-/*   Updated: 2023/01/25 18:50:43 by selbakya         ###   ########.fr       */
+/*   Updated: 2023/01/26 21:27:07 by selbakya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    ft_lstadd_front(t_list **lst, t_list *new)
+void	ft_lstadd_front(t_list **lst, t_list *new)
 {
 	if (!lst)
-        return ;
-    new->next = *lst;
+		return ;
+	new->next = *lst;
 	*lst = new;
 }
