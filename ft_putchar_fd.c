db2eb5d3f8fa9dbd8ff5fe8ea25fc93d946792f1
Shirak Elbakyan <selbakya@c2r8s5.42yerevan.am>
2023-01-26 21:50:21 +0400

    Tests OK Norm OK

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,13 +6,13 @@
 /*   By: selbakya <selbakya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 21:18:48 by selbakya          #+#    #+#             */
-/*   Updated: 2023/01/25 13:30:04 by selbakya         ###   ########.fr       */
+/*   Updated: 2023/01/26 21:50:02 by selbakya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    ft_putchar_fd(char c, int fd)
+void	ft_putchar_fd(char c, int fd)
 {
 	write (fd, &c, 1);
 }
