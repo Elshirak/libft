f77ce0a65a79eec9b8c4fc70b64f2211e52a7a37
Shirak Elbakyan <selbakya@c2r8s5.42yerevan.am>
2023-01-26 21:34:43 +0400

    Test OK Norm OK

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: selbakya <selbakya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 18:59:10 by selbakya          #+#    #+#             */
-/*   Updated: 2023/01/25 19:01:24 by selbakya         ###   ########.fr       */
+/*   Updated: 2023/01/26 21:34:28 by selbakya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 t_list	*ft_lstlast(t_list *lst)
 {
-    if (!lst)
-        return (NULL);
-    while (lst -> next)
-        lst = lst -> next;
-    return (lst);
+	if (!lst)
+		return (NULL);
+	while (lst -> next)
+		lst = lst -> next;
+	return (lst);
 }
