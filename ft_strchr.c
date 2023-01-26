42602643a6818dd0de5e9b495850187a3dede75f
Shirak Elbakyan <selbakya@c2r8s5.42yerevan.am>
2023-01-26 21:58:23 +0400

    Tests OK Norm OK

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,24 +6,24 @@
 /*   By: selbakya <selbakya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 20:50:27 by selbakya          #+#    #+#             */
-/*   Updated: 2023/01/24 14:03:39 by selbakya         ###   ########.fr       */
+/*   Updated: 2023/01/26 21:58:05 by selbakya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char    *ft_strchr(const char *pointer_str, int symbol)
+char	*ft_strchr(const char *pointer_str, int symbol)
 {
-	unsigned char c;
+	unsigned char	c;
 
 	c = (unsigned char)symbol;
 	if (symbol == 0)
 		return ((char *)(pointer_str + ft_strlen(pointer_str)));
-    while (*pointer_str != 0) 
-    {
-        if (*pointer_str == c)
-            return ((char *)pointer_str);
-        pointer_str++;
-    }
-    return (NULL);
+	while (*pointer_str != 0)
+	{
+		if (*pointer_str == c)
+			return ((char *)pointer_str);
+		pointer_str++;
+	}
+	return (NULL);
 }
