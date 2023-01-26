0a13b87134cd13bc7d275a57a15a7fa9f6d90e84
Shirak Elbakyan <selbakya@c2r8s5.42yerevan.am>
2023-01-26 21:33:33 +0400

    Test OK Norm OK

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,20 +6,19 @@
 /*   By: selbakya <selbakya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 19:55:08 by selbakya          #+#    #+#             */
-/*   Updated: 2023/01/25 20:09:02 by selbakya         ###   ########.fr       */
+/*   Updated: 2023/01/26 21:33:11 by selbakya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 void	ft_lstiter(t_list *lst, void (*f)(void *))
-{    
-    if (!f || !lst)
-        return ;
-
-    while (lst)
-    {
-        (*f)(lst -> content);   
-        lst = lst -> next;   
-    }
+{
+	if (!f || !lst)
+		return ;
+	while (lst)
+	{
+		(*f)(lst -> content);
+		lst = lst -> next;
+	}
 }
