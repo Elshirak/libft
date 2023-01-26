130ff6311fa3b5977605290cbf5d231bc77c5dcc
Shirak Elbakyan <selbakya@c2r8s5.42yerevan.am>
2023-01-26 22:05:50 +0400

    Tests OK Norm OK

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: selbakya <selbakya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 15:58:28 by selbakya          #+#    #+#             */
-/*   Updated: 2023/01/25 17:09:01 by selbakya         ###   ########.fr       */
+/*   Updated: 2023/01/26 22:05:13 by selbakya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 
 size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
-
 	size_t	leng_dest;
 	size_t	leng_src;
 	size_t	ls;
@@ -23,13 +22,10 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size)
 
 	if ((!dest ^ !src) && !size)
 		return (0);
-
 	leng_dest = ft_strlen(dest);
 	leng_src = ft_strlen(src);
-
 	if (size == 0 || size <= leng_dest)
 		return (leng_src + size);
-	
 	ls = leng_dest;
 	j = 0;
 	while (j < size - leng_dest - 1 && src[j] != '\0')
@@ -40,5 +36,4 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size)
 	}
 	dest[ls] = '\0';
 	return (leng_dest + leng_src);
-
 }
