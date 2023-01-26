3857cae53479961c26d2c86390ee7927625cc090
Shirak Elbakyan <selbakya@c2r8s5.42yerevan.am>
2023-01-26 21:46:31 +0400

    Tests OK Norm OK

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,22 +6,22 @@
 /*   By: selbakya <selbakya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 22:40:42 by selbakya          #+#    #+#             */
-/*   Updated: 2023/01/19 22:46:51 by selbakya         ###   ########.fr       */
+/*   Updated: 2023/01/26 21:46:15 by selbakya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    *ft_memchr(const void *smth, int symbol, size_t len)
+void	*ft_memchr(const void *smth, int symbol, size_t len)
 {
-    unsigned char *ptr;
-    size_t        iter;
+	unsigned char	*ptr;
+	size_t			iter;
 
-    iter = 0;
-    ptr = (unsigned char *)smth;
-    while (iter != len && ptr[iter] != (unsigned char)symbol)
-        iter++;
-    if (iter == len)
-        return (NULL);
-    return ((void *)(ptr + iter));
+	iter = 0;
+	ptr = (unsigned char *)smth;
+	while (iter != len && ptr[iter] != (unsigned char)symbol)
+		iter++;
+	if (iter == len)
+		return (NULL);
+	return ((void *)(ptr + iter));
 }
