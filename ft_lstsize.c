6e005d374ebe1bbccc96dd7677a82a0c2f1f7f76
Shirak Elbakyan <selbakya@c2r8s5.42yerevan.am>
2023-01-26 21:39:13 +0400

    Test OK Norm OK

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,23 +6,23 @@
 /*   By: selbakya <selbakya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 18:52:21 by selbakya          #+#    #+#             */
-/*   Updated: 2023/01/25 18:57:57 by selbakya         ###   ########.fr       */
+/*   Updated: 2023/01/26 21:38:51 by selbakya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_lstsize(t_list *lst)
+int	ft_lstsize(t_list *lst)
 {
-    size_t  size;
+	size_t	size;
 
-    if (!lst)
-        return (0);
-    size = 0;
-    while(lst)
-    {
-        size++;
-        lst = lst -> next;
-    }
-    return (size);
+	if (!lst)
+		return (0);
+	size = 0;
+	while (lst)
+	{
+		size++;
+		lst = lst -> next;
+	}
+	return (size);
 }
